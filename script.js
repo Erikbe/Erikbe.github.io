d29db93e7aec31c8edeb5ba17cbede71f2196816
ErikBe <erik.berendsen@hotmail.com>
2015-09-29 17:34:53 +0300

    Converted table generation to use javascript instead of being hardscripted into html

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
-$(document).ready(function(){
+$(document).ready(function(){  
+    dynamicNavBar();     
+    fillTable(file);
+});  
+
+var file = [
+["teenus1","kirjeldus1","hind1"],
+["teenus2","kirjeldus2","hind2"],
+["teenus3","kirjeldus3","hind3"],
+["teenus4","kirjeldus4","hind4"]
+];
+
+// This function is used to fill in a table using the file matrix
+function fillTable(matrix){
+    var tableRef = document.getElementById('table1').getElementsByTagName('tbody')[0];
+
+    for (i = 0; i < matrix.length; i++){
+        var newRow   = tableRef.insertRow(tableRef.rows.length);
+        for (j = 0; j < 3; j++){
+            var newCell  = newRow.insertCell(j);
+            var cellContent  = document.createTextNode(matrix[i][j])
+            newCell.appendChild(cellContent);       
+        }
+    } 
+}
+
+function dynamicNavBar(){
     $('.content').css('display','none');
     $('#content1').css('display','');
     $('#menu1').click(function(){
@@ -31,5 +57,4 @@ $(document).ready(function(){
         $('.content').fadeOut(0);
         $('#content6').fadeIn(0);
     });
-});  
-
+}
